a3a82968dfeedc0de4ffa8d5f7eb0f51ec7d41a2
markrobertvandam <markrobertvandam@gmail.com>
2022-05-31 11:46:51 +0200

    Revert "add barplotting"
    This reverts commit a01729e8198e8b0b8b0ed171f18fd96f155d2005.

--- plot_accuracy.py
+++ plot_accuracy.py
@@ -1,4 +1,5 @@
 import argparse
+import matplotlib
 import os
 from matplotlib import pyplot as plt
 
@@ -11,49 +12,94 @@ def parse_args() -> argparse.Namespace:
     """
 
     parser = argparse.ArgumentParser(description="Make plots")
-    parser.add_argument("file", help="log file with guesses", type=str)
+    parser.add_argument("dir", help="maindir with subdirs", type=str)
 
     return parser.parse_args()
 
 
-# function to add value labels
-def addlabels(x,y):
-    for i in range(len(x)):
-        if y[i] > 0:
-            plt.text(i, y[i]+5, y[i], ha = 'center', fontsize=6)
-
-def plot_distribution(
+def plot_accuracy(
     x_values: list,
     y_values: list,
+    names: list,
     save_folder: str,
     name: str,
 ) -> None:
-    fig = plt.figure()
-    plt.bar(x_values, y_values)
-    addlabels(x_values, y_values)
+    fig, ax = plt.subplots()
+    for i in range(len(x_values)):
+        if len(x_values[i]) > 1:
+            plt.plot(x_values[i], y_values[i], label=names[i], linestyle="-")
+        else:
+            # plot horizontal line for fixed agents
+            plt.axhline(y=y_values[i][0], label=names[i], linestyle="--")
+    plt.xlabel("Iterations (n)", fontsize=13)
+    plt.xticks(fontsize=8)
+    for n, label in enumerate(ax.xaxis.get_ticklabels()):
+        if n % 2 != 0:
+            label.set_visible(False)
+    if name == "accuracy":
+        plt.ylabel("Accuracy in %", fontsize=13)
+    elif name == "wins":
+        plt.ylabel("Wins of 1000 games", fontsize=13)
+    elif name == "relative_scores":
+        plt.ylabel("Avg. score diff to winning heuristic", fontsize=13)
+    fig.legend(loc="right", fontsize=8)
     fig.savefig(f"wizard/plots/{save_folder}/{name}_plot")
     plt.close()
 
 
 if __name__ == "__main__":
     args = parse_args()
-    filepath = os.path.join("logs", "comp")
-    filepath = os.path.join(filepath, args.file)
+    x_values = []
+    y_values = []
+    win_totals = []
+    score_avg = []
+    relative_scores = []
+    my_scores = []
+    names = []
+    for directory in sorted(next(os.walk(args.dir))[1]):
+        x = []
+        y = []
+        win_dir = []
+        score = []
+        rel_score = []
+        my_score = []
+        if not directory.startswith("."):
+            names.append(directory)
+            folder_path = os.path.join(args.dir, directory)
+            for filename in sorted(os.listdir(folder_path)):
+                total_corr = 0
+                if filename.endswith(".log"):
+                    f = open(os.path.join(folder_path, filename), "r")
+                    lines = f.readlines()
 
-    f = open(filepath, "r")
-    lines = f.readlines()[-50:]
-    guesses = None
-    actual = None
-    for line in lines:
-        if line.startswith("Guesses:"):
-            # guesses = [int(x.strip("]")) for x in line.split('[')[1].split()]
-            guesses = [int(x) for x in line.split(": ")[1].strip("[]\n").split(", ")]
-        if line.startswith("Actual:"):
-            actual = [int(x) for x in line.split(": ")[1].strip("[]\n").split(", ")]
+                    for line in lines:
+                        if line.startswith("Agents"):
+                            corr = line.split("[")[1].split()[0]
+                            total_corr += int(corr)
+                        if line.startswith("Wins: "):
+                            win_dir.append(int(line[8:].split(",")[0]))
+                        if line.startswith("Scores: "):
+                            score.append(
+                                [round(float(x), 2) for x in line[10:-2].split(",")]
+                            )
+                            rel_score.append(round(score[-1][0] - max(score[-1]), 2))
+                            my_score.append(score[-1][0])
+                    iters = lines[2][7:]
+                    accuracy = total_corr / 20000
 
-    if guesses is not None:
-        print(sum(guesses))
-        plot_distribution([x for x in range(0, 21)], guesses, "bar-test", "guesses")
-    if actual is not None:
-        print(sum(actual))
-        plot_distribution([x for x in range(0, 21)], actual, "bar-test", "actual")
+                    x.append(iters)
+                    y.append(accuracy)
+
+            x_values.append(x)
+            y_values.append(y)
+            win_totals.append(win_dir)
+            score_avg.append(score)
+            relative_scores.append(rel_score)
+            my_scores.append(my_score)
+
+    print(score_avg)
+    print(relative_scores)
+    print(my_scores)
+    plot_accuracy(x_values, y_values, names, "test_filter", "accuracy")
+    plot_accuracy(x_values, win_totals, names, "test_filter", "wins")
+    plot_accuracy(x_values, relative_scores, names, "test_filter", "relative_scores")
