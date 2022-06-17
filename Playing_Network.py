1788b4cc7d64f132cf8e13e30b92b632c1a812fa
M.R.van.Dam.1@student.rug.nl <markrobertvandam@gmail.com>
2022-06-17 23:19:09 +0200

    updated DQN commit

--- Playing_Network.py
+++ Playing_Network.py
@@ -102,8 +102,7 @@ class PlayingNetwork:
         # When using target network, query it, otherwise main network should be queried
         new_current_states = np.array([Playing_Agent.PlayingAgent.key_to_state(self.input_size, transition[3])
                                        for transition in minibatch])
-        future_qs_list = self.model.predict(new_current_states)
-        future_q_vals = self.target_model.predict(new_current_states)
+        future_qs_list = self.target_model.predict(new_current_states)
 
         X = []
         y = []
@@ -117,11 +116,8 @@ class PlayingNetwork:
             # If not a terminal state, get new q from future states, otherwise set it to 0
             # almost like with Q Learning, but we use just part of equation here
             if not done:
-                # best action predicted by online model
-                best_future_action = np.argmax(future_qs_list[index])
-
                 # evaluation of best action done by target model
-                max_future_q = future_q_vals[index][best_future_action]
+                max_future_q = np.max(future_qs_list[index])
                 new_q = reward + DISCOUNT * max_future_q
             else:
                 new_q = reward
@@ -161,7 +157,7 @@ class PlayingNetwork:
                        shuffle=False)
 
         if self.q_memory_counter % 21000 == 0:
-            save_name = "DDQN"
+            save_name = "DQN"
             print("Saving q_mem...")
             f = open(f"plots/q_plots/values_{save_name}", "w")
             f.write(f"{self.avg_q_memory}\n")
