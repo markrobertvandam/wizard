2b73f83c886047ae189e99f4af9b65de29de9746
markrobertvandam <markrobertvandam@gmail.com>
2022-06-30 00:23:11 +0200

    write bugfix

--- Playing_Agent.py
+++ Playing_Agent.py
@@ -105,7 +105,7 @@ def write_state(play_state: np.ndarray, output_path: str, input_size: int, type_
     if input_size == 313:
         f.write(
             "possible cards player2: "
-            + str(np.nonzero(~(play_state[current_pos:current_pos+60] != 0))[0].tolist())
+            + str(np.nonzero(play_state[current_pos:current_pos+60])[0].tolist())
             + "\n"
         )
         f.write(
