bugfix new score-based reward and add softmax guesser

Guessing_Agent.py
@@ -14,11 +14,12 @@ MINIBATCH_SIZE = 32  # How many steps (samples) to use for training
 
 # Agent class
 class GuessingAgent:
-    def __init__(self, input_size, guess_max):
+    def __init__(self, input_size, guess_max, soft_guess=False):
 
         self.input_size = input_size
         self.guess_max = guess_max
         self.accuracy = 0.02
+        self.soft_guess = soft_guess
 
         # Main model
         self.model = self.create_model()
@@ -83,6 +84,26 @@ class GuessingAgent:
             verbose=0,
             shuffle=False,
         )
+
+    def get_guess(self, state) -> int:
+        """
+        Get guess using get_qs
+        """
+        output = self.get_qs(state)
+
+        if self.soft_guess:
+            print("Using soft guess..")
+            probs = np.cumsum(output)
+            rand = random.uniform(0, 1)
+            print(rand)
+            for i in range(len(output)):
+                if rand < probs[i]:
+                    break
+            guess = i
+        else:
+            print("Using argmax..")
+            guess = np.argmax(output)
+        return guess
 
     # Queries main network for Q values given current observation space (environment state)
     def get_qs(self, state):

player.py
@@ -19,6 +19,7 @@ class Player:
         epsilon=None,
         player_epsilon=None,
         verbose=False,
+        soft_guess=False,
     ) -> None:
         self.verbose = verbose
         self.player_name = player_name
@@ -37,6 +38,7 @@ class Player:
         if self.player_type.startswith("learn"):
             self.play_agent = play_agent
             self.player_epsilon = player_epsilon
+            self.soft_guess = soft_guess
 
         # for playing state
         self.possible_cards_one = [1] * 60
@@ -247,9 +249,7 @@ class Player:
                 self.current_state = state_space
                 if np.random.random() > self.epsilon:
                     # Get action from Q table
-                    self.player_guesses = np.argmax(
-                        self.guess_agent.get_qs(state_space)
-                    )
+                    self.player_guesses = self.guess_agent.get_guess(state_space)
                 else:
                     # Get random action
                     self.player_guesses = random.randrange(max_guesses + 1)